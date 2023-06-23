a1795eacb9991339cc8c7d9324633b5e97262230
Shubham Upadhyay <shubhamup47@gmail.com>
2023-06-23 23:54:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,32 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	let price = [];
+
+  let a = document.querySelectorAll(".price");
+  // price.push(a[1].innerText);
+
+  for(let i=0; i<a.length; i++){
+    price.push(a[i].innerText);
+  }
+  let total = 0;
+  for(let i=0; i<a.length; i++)
+  {
+    total += parseInt(price[i]);
+  }
+  console.log(total)
+
+ let lr = document.createElement("tr");
+
+  let lc = `
+    <td>Total</td>
+    <td>${total}</td>
+   `;
+   lr.innerHTML = lc;
+   let t = document.getElementById("myTable");
+   t.append(lr)
+
+   getSumBtn.classList.add("hide")
 };
 
 getSumBtn.addEventListener("click", getSum);
-
